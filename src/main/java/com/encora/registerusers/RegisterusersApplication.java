ea39523c18eb26a8fa62498523c544943aea8ab2
alobaton <alobaton.restrepo@gmail.com>
2021-09-04 13:04:26 -0500

    base del proyecto

---
+++ src/main/java/com/encora/registerusers/RegisterusersApplication.java
@@ -0,0 +1,13 @@
+package com.encora.registerusers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterusersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisterusersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/encora/registerusers/RegisterusersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.encora.registerusers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterusersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
